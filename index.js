e84c380c3c98ee849a0eabc5ee32c4a6c0e0470e
Mohammad Shakib <mdshakib105100@gmail.com>
2022-09-08 00:33:50 +0600

    ready for vercel deploy

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
-const userRoute = require("./routes/users.route.js")
+const userRoute = require("./routes/users.route.js ")
 
 
 app.use(cors());
